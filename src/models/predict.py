f5b61cfa1d1d1b9ebece6aa3ee228e85d7ec009d
juanvieira <juanvieira1@yahoo.com>
2024-11-11 11:27:53 -0300

    test(model): retrain test

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,5 +1,5 @@
 import mlflow
-logged_model = 'mlartifacts/700678007511616763/48ffe9140fe84fd6ab247b80bce57ad2/artifacts/model'
+logged_model = 'src/models/mlartifacts/700678007511616763/48ffe9140fe84fd6ab247b80bce57ad2/artifacts/model'
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.pyfunc.load_model(logged_model)
@@ -7,7 +7,7 @@ loaded_model = mlflow.pyfunc.load_model(logged_model)
 # Predict on a Pandas DataFrame.
 import pandas as pd
 
-data = pd.read_csv('../../data/preprocessed/preprocessed_data.csv')
+data = pd.read_csv('data/preprocessed/preprocessed_data.csv')
 features = ['personal_loan', 'education', 'securities_account',
             'cd_account', 'online', 'age_bracket_name_Baby boomers',
             'age_bracket_name_Generation X', 'age_bracket_name_Generation Z',
@@ -17,4 +17,4 @@ features = ['personal_loan', 'education', 'securities_account',
 predicted = loaded_model.predict(data[features])
 
 data['predicted'] = predicted
-data.to_csv('../../reports/approval_results.csv', index=False)
+data.to_csv('reports/approval_results.csv', index=False)
